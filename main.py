5bebef612c1747b605ee53cf4f1bc54c3233cf37
holyrobots <jks.sheesley@gmail.com>
2025-06-09 22:23:47 -0500

    Added plsayer rotation and movement

--- main.py
+++ main.py
@@ -7,15 +7,21 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
     while True:
+        dt = clock.tick(60) / 1000
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(000000)
-        player.draw(screen)
+        for thing in drawable:
+            thing.draw(screen)
+        updatable.update(dt)
         pygame.display.flip()
-        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
